7ab87bea145c6fa52a68239e8092d48b4d80c4e6
IbnuAhsani <ibnu.muhari@gmail.com>
2018-03-14 23:15:31 +0700

    Create Seeder to Automatically Input Data for Users Table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+          'name' => str_random(10),
+          'email' => str_random(10).'@gmail.com',
+          'password' => 'secret',
+          'api_token' => str_random(10),
+      ]); 
+
+      DB::table('users')->insert([
+          'name' => str_random(10).'@gmail.com',
+          'email' => str_random(10).'@gmail.com',
+          'password' => '12345',
+          'api_token' => str_random(10),
+      ]); 
+    }
+}
