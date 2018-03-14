0ace5ade122d60b7604107177112747ba32ceb7b
IbnuAhsani <ibnu.muhari@gmail.com>
2018-03-14 23:11:45 +0700

    Create User Model

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'name', 'email', 'password', 'api_token'
     ];
 
     /**
@@ -27,6 +27,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password',
+        'password', 'api_token', 'remember_token'
     ];
 }
