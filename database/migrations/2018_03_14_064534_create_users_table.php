8646f18ed6f9820cc4f908a75d15083ce78c85f6
IbnuAhsani <ibnu.muhari@gmail.com>
2018-03-14 23:12:57 +0700

    Create CreateUsersTable Migration

---
+++ database/migrations/2018_03_14_064534_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Query to create a new 'Users' table
+        Schema::create('users', function(Blueprint $table)
+            {
+                $table->increments('id');
+                $table->string('email')->unique();
+                $table->string('name');
+                $table->string('password', 20);
+                $table->string('api_token', 60)->unique();
+                $table->rememberToken();
+                $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // If 'Users' table exists, a rollback
+        // will undo it
+        Schema::dropIfExists('users');
+    }
+}
