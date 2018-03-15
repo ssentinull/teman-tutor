63079ed19fe24cb185ceb9bb70620c0001ef94d8
IbnuAhsani <ibnu.muhari@gmail.com>
2018-03-15 20:55:43 +0700

    Create CreateGroupsTable & CreateGroup_UserTable Migration

---
+++ database/migrations/2018_03_15_025204_create_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('desc');
+            $table->integer('course_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
