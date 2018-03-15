00847832ccb93e3ec13f31603cf08fea563b0126
IbnuAhsani <ibnu.muhari@gmail.com>
2018-03-15 20:52:46 +0700

    Create Group Model

---
+++ app/Group.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class Group extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'desc', 'course_id',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+
+    // Create a Many-To-Many Relationship
+    // with 'Users' Table
+    public function users(){
+
+        return $this->belongsToMany('App\User');
+
+    }
+}
