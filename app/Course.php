933eba4c515baa72db1341a00eb1ce09c4810ee8
IbnuAhsani <ibnu.muhari@gmail.com>
2018-03-18 18:31:21 +0700

    Create CRUD API for Courses

---
+++ app/Course.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class Course extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    protected $table = 'Courses';
+    
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'desc',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+
+    // Create a One-To-Many Relationship
+    // with 'Groups' Table
+    public function groups(){
+
+        return $this->hasMany('App\Group');
+    
+    }
+}
---
+++ app/Http/Controllers/CoursesController.php
@@ -0,0 +1,75 @@
+<?php namespace App\Http\Controllers;
+
+	use App\Course;
+	use App\Http\Controllers\Controller;
+	use Illuminate\Http\Request;
+	use Illuminate\Hashing\BcryptHasher;
+	use DB;
+
+	class CoursesController extends Controller
+		{
+			// Limiting the Methods that could be
+			// used without an 'api_token'
+			// function __construct()
+			// 	{
+			// 		$this->middleware('auth');
+			// 	}
+
+			//  Create a new Course based on the input
+			public function add(Request $request)
+				{
+					// The id of the User who created this
+					// group should be included in the $request
+					
+					// dd($data->all());
+					
+					$course = Course::create($request->all());
+
+					return response()->json($course);
+				}
+
+			//  View a Course based on the given $id
+			public function view($id)
+				{
+					$course = Course::find($id);
+
+					return response()->json($course);
+				}
+
+			//  Edit a Course based on the given $id
+			public function edit(Request $request, $id)
+				{
+					$course = Course::find($id);
+					$course->update($request->all());
+
+					return response()->json($course);
+				}
+
+			//  Delete a Course based on the given $id
+			public function delete($id)
+				{
+					$course = Course::find($id);
+					$course->delete();
+
+					return response()->json('Removed successfully.');
+				}
+
+			//  Display all the Course in the 'Courses' table
+			public function allCourses()
+				{
+					//   $courses = Course::all();
+					$courses = DB::table('groups')->get();
+
+					return response()->json($courses);
+				}
+
+			// Get the Groups that studies the
+			// Course with the given $id 
+			public function groups($id)
+				{
+					$groups = Course::find($id)->groups;
+
+					return response()->json($groups);
+				}
+		}
+ ?>
