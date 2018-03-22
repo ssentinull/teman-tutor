7ebc472560d12dd06edf241a2d3d2a0e24e6b26a
IbnuAhsani <ibnu.muhari@gmail.com>
2018-03-22 21:03:03 +0700

    Create CRUD API for Messages

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,45 @@
+<?php namespace App\Http\Controllers;
+
+	use DB;
+	use App\Message;
+	use Illuminate\Http\Request;
+	use App\Http\Controllers\Controller;
+	use Illuminate\Hashing\BcryptHasher;
+	use Illuminate\Support\Facades\Input;
+
+	class MessagesController extends Controller
+		{
+			// Limiting the Methods that could be
+			// used without an 'api_token'
+			function __construct()
+				{
+					$this->middleware('auth');
+				}
+
+			// Create a new Message
+			public function add(Request $request)
+				{
+					$message = Message::create($request->all());
+					return response()->json($message);
+				}
+
+			// View Messages that was sent from within
+			// the Group with the given $group_id
+			public function view($group_id)
+				{
+					$messages = Message::where('group_id', $group_id)
+														->sortBy('created_at')
+														->get();
+					return response()->json($messages);
+				}
+
+			// Delete a Message with the given $id in
+			// the Group with the given $group_id
+			public function delete($id)
+				{
+					$message = Message::find($id)->delete();
+
+					return response()->json('Removed successfully.');
+				}
+		}
+ ?>
